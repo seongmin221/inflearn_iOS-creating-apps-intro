5a0b2878d11abc70f5072e6a27a11b8a4b58d984
seongmin221 <seongmin221@naver.com>
2022-04-23 16:20:22 +0900

    practice proj. SimpleCenter

---
+++ SimpleCenter/SimpleCenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCenter
+//
+//  Created by 이성민 on 2022/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
